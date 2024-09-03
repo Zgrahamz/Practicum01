9c8da0f8850d882244e9d41d3d8db3c30a7bddd3
Zgrahamz <graham_zach@outlook.com>
2024-09-03 09:50:32 -0400

    Update PersonGenerator.java

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -57,7 +57,7 @@ public class PersonGenerator {
         // (may need to adjust for other IDE)
         // Not sure if the toolkit is thread safe...
         File workingDirectory = new File(System.getProperty("user.dir"));
-        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonData.txt");
+        Path file = Paths.get(workingDirectory.getPath() + "/src/PersonData.txt");
 
         try
         {
